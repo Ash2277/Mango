97451309576a44cdbacdd9146c7dbb50f44f60cb
Diptonil <diptonilr@gmail.com>
2021-07-09 15:37:07 +0530

    Font added, skeleton layout for login made

--- app/src/main/java/com/example/grocery_app/Homepage.java
+++ app/src/main/java/com/example/grocery_app/Homepage.java
@@ -1,33 +1,21 @@
 package com.example.grocery_app;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.Button;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceDetectionClient;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Homepage extends AppCompatActivity
 {
-    public static final String TAG = "CurrentLocNearByPlaces";
-    private static final int LOC_REQ_CODE = 1;
-
-    protected GeoDataClient geoDataClient;
-    protected PlaceDetectionClient placeDetectionClient;
+    Button b;
 
 
     @Override
@@ -35,5 +23,15 @@ public class Homepage extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        b = findViewById(R.id.log);
+        b.setOnClickListener(v -> click());
     }
+
+    public void click()
+    {
+        Intent i = new Intent(Homepage.this, Login.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/grocery_app/Login.java
+++ app/src/main/java/com/example/grocery_app/Login.java
@@ -1,14 +1,31 @@
 package com.example.grocery_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity
 {
+    TextView name, address, mobile, email;
+    Button submit;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_login);
+
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(v -> click());
+
+    }
+
+    public void click()
+    {
+        name = findViewById(R.id.name);
+        address = findViewById(R.id.name);
+        mobile = findViewById(R.id.name);
+        email = findViewById(R.id.name);
     }
 }
