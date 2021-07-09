49b1cd56f1e2d791748229a49bfe0b60bd285916
Ash2277 <78946808+Ash2277@users.noreply.github.com>
2021-07-09 11:23:05 +0530

    Logo

---
+++ app/src/main/java/com/example/grocery_app/LogoScreen.java
@@ -0,0 +1,14 @@
+package com.example.grocery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_screen);
+    }
+}
