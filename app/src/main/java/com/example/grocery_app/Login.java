588e4b9c8f380484cfa9f9ad512f17202f5eb163
Diptonil <diptonilr@gmail.com>
2021-07-09 13:52:58 +0530

    API Key (#don't alter the java files)

---
+++ app/src/main/java/com/example/grocery_app/Login.java
@@ -0,0 +1,14 @@
+package com.example.grocery_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
