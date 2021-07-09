2f49f70b5de036222d9e203d52614ec3ecf0af7a
Aisiriiiiiii <aisirisrinath@gmail.com>
2021-07-09 12:45:37 -0700

    sell veggies

---
+++ app/src/main/java/com/example/grocery_app/ItemsRegistrationVeggies.java
@@ -0,0 +1,14 @@
+package com.example.grocery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemsRegistrationVeggies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_registration_veggies);
+    }
+}
