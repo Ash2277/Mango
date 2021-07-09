b918f5898b4b0089e03bd4da38d64ca9789eb436
Ash2277 <78946808+Ash2277@users.noreply.github.com>
2021-07-09 12:49:13 +0530

    Logo splashscreen

--- app/src/main/java/com/example/grocery_app/LogoScreen.java
+++ app/src/main/java/com/example/grocery_app/LogoScreen.java
@@ -2,13 +2,27 @@ package com.example.grocery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LogoScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(LogoScreen.this, Homepage.class);
+                LogoScreen.this.startActivity(mainIntent);
+                LogoScreen.this.finish();
+            }
+        } ,SPLASH_DISPLAY_LENGTH);
+
     }
 }
