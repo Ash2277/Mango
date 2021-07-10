9afb03abf55c24d5ed815d6dd35bd9d14e701435
Diptonil <diptonilr@gmail.com>
2021-07-10 10:13:25 +0530

    Retailer Registration

--- app/src/main/java/com/example/grocery_app/ItemsRegistration.java
+++ app/src/main/java/com/example/grocery_app/ItemsRegistration.java
@@ -2,15 +2,37 @@ package com.example.grocery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.R;
 
-public class ItemsRegistration extends AppCompatActivity {
+public class ItemsRegistration extends AppCompatActivity
+{
 
+    ImageView v1, v2;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_registration);
+
+        v1 = findViewById(R.id.imageView9);
+        v2 = findViewById(R.id.imageView);
+
+        v1.setOnClickListener(v -> click());
+        v2.setOnClickListener(v -> click2());
+    }
+
+    public void click()
+    {
+        Intent i = new Intent(ItemsRegistration.this, ItemsRegisterationFruits.class);
+        startActivity(i);
+    }
+    public void click2()
+    {
+        Intent i = new Intent(ItemsRegistration.this, ItemsRegistrationVeggies.class);
+        startActivity(i);
     }
 }
