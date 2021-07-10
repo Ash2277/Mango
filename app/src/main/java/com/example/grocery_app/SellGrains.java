0060950cac0e0bf9ca143b3f8e6556376c50b65e
Aisiriiiiiii <aisirisrinath@gmail.com>
2021-07-10 02:13:09 -0700

    sell rice, grains, flour and thank you page

---
+++ app/src/main/java/com/example/grocery_app/SellGrains.java
@@ -0,0 +1,13 @@
+package com.example.grocery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellGrains extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_grains);
+    }
+}
